66e6dc3a5f83486d37664a30e910c1d8148f7f08
julio cabrera <juliocabrera820@gmail.com>
2021-05-09 00:11:33 -0500

    fix: show error message

--- lux/commands/find_user.rb
+++ lux/commands/find_user.rb
@@ -2,6 +2,7 @@
 
 require 'lux/services/github'
 require 'lux/utils/parser'
+require 'lux/utils/error_builder'
 
 module Lux
   module Commands
@@ -10,6 +11,11 @@ module Lux
         github_service = Services::Github.call
         user = match[:user]
         response = github_service[:find_user].call(user)
+        if (response['message'] == "Not Found")
+          error_block = Utils::ErrorBuilder.build_error_block(user)
+          slack_client = Slack::Web::Client.new
+          slack_client.chat_postMessage(channel: data.channel, blocks: error_block)
+        end
         parser = Utils::Parser.call
         user_data = parser[:parse_response].call(response)
         client.say(channel: data.channel, text: user_data['html_url'])
